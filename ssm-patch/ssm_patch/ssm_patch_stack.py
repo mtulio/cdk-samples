0666412199fd3a8d69b05e6798c8e238a3967b0b
Marco Braga <marco.tulio@mtulio.eng.br>
2020-09-15 17:10:29 -0300

    feat/ssm-patch: add Association for doc AWS-RunPatchBaseline

--- ssm-patch/ssm_patch/ssm_patch_stack.py
+++ ssm-patch/ssm_patch/ssm_patch_stack.py
@@ -13,11 +13,11 @@ class SsmPatchStack(core.Stack):
         ssm_ass = _ssm.CfnAssociation(
             self, 'inventory-all-instances',
             name='AWS-GatherSoftwareInventory',
-            document_version="$DEFAULT",
+            #document_version="1",  # there is a bug when creating resource with this arg, to avoid err on creation it should be commented
             association_name='inventory-all-instances',
             apply_only_at_cron_interval=False,
             schedule_expression="rate(30 minutes)",
-            compliance_severity="CRITICAL",
+            #compliance_severity="CRITICAL",
             targets=[
                  _ssm.CfnAssociation.TargetProperty(
                      key='InstanceIds', values=['*']
@@ -37,6 +37,27 @@ class SsmPatchStack(core.Stack):
         ssm_ass.add_override('Properties.Parameters.windowsRegistry', [''])
         ssm_ass.add_override('Properties.Parameters.windowsRoles', ['Enabled'])
         ssm_ass.add_override('Properties.Parameters.windowsUpdates', ['Enabled'])
+
+        # Create association for Patch Baseline
+        ssm_assPatch = _ssm.CfnAssociation(
+            self, 'patch-scan-all-instances',
+            name='AWS-RunPatchBaseline',
+            #document_version="1",  # there is a bug when creating resource with this arg, to avoid err on creation it should be commented
+            association_name='patch-scan-all-instances',
+            apply_only_at_cron_interval=False,
+            #schedule_expression="rate(720 minutes)",
+            compliance_severity="HIGH",
+            targets=[
+                 _ssm.CfnAssociation.TargetProperty(
+                     key='InstanceIds', values=['*']
+                )
+            ],
+            max_concurrency="1000",
+            max_errors="1000"
+        )
+        ssm_assPatch.add_override('Properties.Parameters.Operation', ['Scan'])
+        ssm_assPatch.add_override('Properties.Parameters.RebootOption', ['NoReboot'])
+
 
         # # 2 - create maintenance window
         # # 3 - configure patching
